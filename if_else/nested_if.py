db9349a8d926433d941f6489c0e4ed655596169d
DeViL8077 <mann6657@gmail.com>
2025-01-30 03:23:57 +0000

    add nested if-else structure for comparing three numbers

---
+++ if_else/nested_if.py
@@ -0,0 +1,13 @@
+a=int(input("Enter number :"))
+b=int(input("Enter Number :"))
+c=int(input("Enter Number :"))
+if(a>b):
+    if(a>c):
+        print("a is greater")
+    else:
+        print("c is greater")
+else:
+    if(b>c):
+        print("b is greater")
+    else:
+        print("c is greater")   
